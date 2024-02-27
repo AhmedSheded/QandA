88a42baedb67f8710b80cddcc973bbfffbab9c07
Ahmed Sheded <ahmed.shededmmm@gmail.com>
2024-02-27 21:19:57 +0200

    modified test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 
-image_path = 'test/imgs/6.jpg'
+image_path = 'test/imgs/4.png'
 
-language = 'l'
+language = 'm'
 
 # API endpoint URL
 # url = 'http://127.0.0.1:5000/ocr'
-url = 'http://64.23.133.179:5000/ocr'
+url = 'http://64.23.164.38:5000/ocr'
 
 files = {'image': open(image_path, 'rb')}
 data = {'language': language}
